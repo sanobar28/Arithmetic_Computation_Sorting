ed5e6f9b20cb58a173ba25b8a2af7229c86fea8f
sanobar28 <sanobar.mujawar@gmail.com>
2021-05-15 23:03:22 +0530

    [Sanobar] added dictionary to store results

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+declare -A results
+
 read -p "Enter value of a" a
 read -p "Enter value of b" b
 read -p "Enter value of c" c
@@ -6,4 +8,6 @@ read -p "Enter value of c" c
 op1=$(( a+b*c ))
 op2=$(( a*b+c ))
 op3=$(( c+a/b ))
-op4=&(( a%b+c ))
+op4=$(( a%b+c ))
+
+results=( ["re1"]=$op1 ["re2"]=$op2 ["re3"]=$op3 ["re4"]=$op4 )
