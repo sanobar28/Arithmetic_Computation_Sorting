194b412810473e2f6b1c99fc80dcc0f377f103ab
sanobar28 <sanobar.mujawar@gmail.com>
2021-05-16 09:58:29 +0530

    [Sanobar] added result in descending & ascending

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -23,5 +23,40 @@ arr[((val))]="$op4"
 
 echo "results in array- " ${arr[@]}
 
-echo "Results in descending-  " ${arr[@]} | sort -r
+for ((i = 0; i<4; i++)) 
+do
+      
+    for((j = 0; j<4-i-1; j++)) 
+    do
+      
+        if [ ${arr[j]} -lt ${arr[$((j+1))]} ] 
+        then 
+            temp=${arr[j]} 
+            arr[$j]=${arr[$((j+1))]}   
+            arr[$((j+1))]=$temp 
+        fi
+    done
+done
+  
+echo "Array in descending order :"
+echo ${arr[@]} 
 
+
+
+for ((i = 0; i<4; i++))
+do
+
+    for((j = 0; j<4-i-1; j++))
+    do
+
+        if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
+        then
+            temp=${arr[j]}
+            arr[$j]=${arr[$((j+1))]}
+            arr[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Array in Ascending order :"
+echo ${arr[@]}
